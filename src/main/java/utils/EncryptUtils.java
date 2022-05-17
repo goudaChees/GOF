7dbb98b6f10d0db45f98bba10a65ef46779fc95c
Yunji <yjjung9494@hanmail.net>
2022-05-17 12:10:37 +0900

    dao, dto, utils folder created

---
+++ src/main/java/utils/EncryptUtils.java
@@ -0,0 +1,23 @@
+package utils;
+
+import java.security.MessageDigest;
+
+public class EncryptUtils {
+	public String SHA512(String pw) {
+		try{
+			MessageDigest digest = MessageDigest.getInstance("SHA-512");
+			byte[] hash = digest.digest(pw.getBytes("UTF-8"));
+			StringBuffer hexString = new StringBuffer();
+
+			for (int i = 0; i < hash.length; i++) {
+				String hex = Integer.toHexString(0xff & hash[i]);
+				if(hex.length() == 1) hexString.append('0');
+				hexString.append(hex);
+			}
+			return hexString.toString();
+			
+		} catch(Exception ex){
+			throw new RuntimeException(ex);
+		}
+	}
+}
